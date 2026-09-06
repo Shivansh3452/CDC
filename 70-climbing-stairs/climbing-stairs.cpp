76429ed9104fda213d0dcc9aca3629dc4d72ad80
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-09-06 22:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int t[46];
-    int solve(int n){
-        if(n<0)     //no way for less than 0 stairs
-            return 0;
+    int helper(int t[],int n){
+        if(n<=2)
+            return n;
         if(t[n]!=-1)
             return t[n];
-        if(n==0)
-            return 1;   //only one way for 0 stairs
-        int onestep=solve(n-1);
-        int twostep=solve(n-2);
-        return t[n]=onestep+twostep;
+        return t[n]=helper(t,n-1)+helper(t,n-2);
     }
     int climbStairs(int n) {
         memset(t,-1,sizeof(t));
-       return solve(n);
+        return helper(t,n);
     }
 };
