0475b2ff8ee7c4da88115a3b9013bd2dfaa0b4b6
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-09-08 22:07:54 +0530

    Time: 28 ms (33.03%) | Memory: 95.5 MB (5.04%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int> left(n,0);
-        vector<int> right(n,0);
-        stack<int> s;
-        //Right Smaller
-        for(int i=n-1;i>=0;i--){
-            while(s.size()>0 && heights[s.top()]>=heights[i]){
-                s.pop();
-            }
-            right[i]=s.empty()?n:s.top();
-            s.push(i);
-        }
-        while(!s.empty()){
-            s.pop();
-        }
-        //Left Smaller
+    vector<int> prevSmaller(vector<int>& nums){
+        vector<int> ans;
+        stack<int> st;
+        int n=nums.size();
         for(int i=0;i<n;i++){
-            while(s.size()>0 && heights[s.top()]>=heights[i]){
-                s.pop();
-            }
-            left[i]=s.empty()? -1:s.top();
-            s.push(i);
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            int width=right[i]-left[i]-1;
-            int currArea=heights[i]* width;
-            ans=max(ans,currArea);
+            while(!st.empty()&&nums[st.top()]>=nums[i])
+                st.pop();
+            int ele=(st.empty())?-1:st.top();
+            ans.push_back(ele);
+            st.push(i);
         }
         return ans;
+    }
+    vector<int> nextSmaller(vector<int>& nums){
+        vector<int> ans;
+        stack<int> st;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&nums[st.top()]>=nums[i])
+                st.pop();
+            int ele=(st.empty())?n:st.top();
+            ans.push_back(ele);
+            st.push(i);
+        }
+        reverse(begin(ans),end(ans));
+        return ans;
+    }
+    int largestRectangleArea(vector<int>& nums) {
+        int res=0;
+        int n=nums.size();
+        vector<int> prev=prevSmaller(nums);
+        vector<int> next=nextSmaller(nums);
+        for(int i=0;i<n;i++){
+            int curr=(next[i]-prev[i]-1)*nums[i];
+            res=max(res,curr);
+        }
+        return res;
     }
 };
